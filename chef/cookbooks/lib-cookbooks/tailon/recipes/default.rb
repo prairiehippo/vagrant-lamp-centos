ee4556301335e881a9a8592bb6a6c80dae5794d5
rcourtna <abcczc@gmail.com>
2014-02-24 07:47:31 -0700

    run tailon in background so as not to hold up vagrant provision

--- chef/cookbooks/lib-cookbooks/tailon/recipes/default.rb
+++ chef/cookbooks/lib-cookbooks/tailon/recipes/default.rb
@@ -11,12 +11,12 @@ bash "tailon_on_start" do
   user "root"
     cwd "/tmp"
     code <<-EOT
-       echo "touch /tmp/berr.log && touch /tmp/BenevityClient.txt && chmod 666 /tmp/berr.log && chmod 666 /tmp/BenevityClient.txt && nohup tailon --bind 33.33.33.20:8080 --allow-transfers -f /tmp/BenevityClient.txt /var/log/httpd/*{access,error}.log /tmp/berr.log /var/log/messages" >> /etc/rc.local
+       echo "touch /tmp/berr.log && touch /tmp/BenevityClient.txt && chmod 666 /tmp/berr.log && chmod 666 /tmp/BenevityClient.txt && nohup tailon --bind 33.33.33.20:8080 --allow-transfers -f /tmp/BenevityClient.txt /var/log/httpd/*{access,error}.log /tmp/berr.log /var/log/messages &" >> /etc/rc.local
     EOT
     not_if "cat /etc/rc.local | grep tailon"
 end
 
 execute "tailon" do
-  command "touch /tmp/berr.log && touch /tmp/BenevityClient.txt && chmod 666 /tmp/berr.log && chmod 666 /tmp/BenevityClient.txt && nohup tailon --bind 33.33.33.20:8080 --allow-transfers -f /tmp/BenevityClient.txt /var/log/httpd/*{access,error}.log /tmp/berr.log /var/log/messages"
+  command "touch /tmp/berr.log && touch /tmp/BenevityClient.txt && chmod 666 /tmp/berr.log && chmod 666 /tmp/BenevityClient.txt && nohup tailon --bind 33.33.33.20:8080 --allow-transfers -f /tmp/BenevityClient.txt /var/log/httpd/*{access,error}.log /tmp/berr.log /var/log/messages &"
   not_if "ps -Al | grep tailon"
 end
